3fedc1d7eb08948c798a9d86820d5b71da74c097
Kasun3865 <ict21945@fot.sjp.ac.lk>
2025-02-12 23:51:17 +0530

    Add registration screen
    - added new png logo image
    - added registration screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:unimo_mobile_app/Screens/login_screen.dart';
+import 'package:unimo_mobile_app/Screens/signin_screen.dart';
+import 'package:unimo_mobile_app/onboarding_screen/onboarding1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,10 +31,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 255, 255, 255)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoginScreen(),
     );
   }
 }
